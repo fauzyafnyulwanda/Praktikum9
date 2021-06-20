86cf3bfebc0a4611e270c5c019663b6fa198a69f
fauzyafnyulwanda <afniulwanda12@gmai.com>
2021-06-20 20:14:19 +0700

    first commit

---
+++ main_dashboard.php
@@ -0,0 +1,2 @@
+<h2>Selamat Datang <?php echo $_SESSION['user_name'];?></h2>
+<h2> Enjoy Your music :) </h2>
---
+++ main_index.php
@@ -0,0 +1,3 @@
+<h2>Welcome to the music world </h2>
+<h2> Let's Have Fun Together    </h2>
+<h2> Enjoy Your music :) </h2>
